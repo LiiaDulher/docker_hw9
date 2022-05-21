73f86cd75e94d324459e41bb3f8e465fc8abefb8
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-05-21 10:51:16 +0300

    Update spark_transactions.py

--- spark_transactions.py
+++ spark_transactions.py
@@ -1,10 +1,20 @@
 from pyspark.sql import SparkSession
 
 
+class SparkApp:
+    def __init__(self, name):
+        self.spark = SparkSession.builder.appName(name).getOrCreate()
+
+    def process_file(self, file_name):
+        df = self.spark.read.csv(file_name)
+        print("Number of lines in file:", df.count())
+
+
 def main():
-    spark = SparkSession.builder.appName('SparkTransactionsProject').getOrCreate()
-    df = spark.read.csv("PS_20174392719_1491204439457_log.csv")
-    print(df.count())
+    spark_name = 'SparkTransactionsProject'
+    file_name = 'PS_20174392719_1491204439457_log.csv'
+    spark_app = SparkApp(spark_name)
+    spark_app.process_file(file_name)
 
 
 if __name__ == "__main__":
